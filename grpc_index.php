affcb12419b9c1e0db5f1e5536da32d0e6c3656b
Juan Rael <juan@qlogic.io>
2018-10-26 15:15:06 -0400

    Grpc Stackdriver

--- grpc_index.php
+++ grpc_index.php
@@ -10,10 +10,21 @@ use OpenCensus\Trace\Tracer;
 use OpenCensus\Trace\Exporter\FileExporter;
 use Google\ApiCore\ApiException;
 use OpenCensus\Version;
+use OpenCensus\Trace\Exporter\StackdriverExporter;
 
 
 echo Version::VERSION."\n";
+/*
+$traceClient = new TraceClient([
+    'projectId' => getenv('PROJECT_ID'),
+]);
+$tracerClient = [
+    'client' => $traceClient
+];
 
+$exporter = new StackdriverExporter( $tracerClient );
+$trace = $traceClient->trace();
+*/
 $exporter = new FileExporter( 'logs/grpc_bigtable_' . ( new DateTime() )->format('dmYGisu') . '.json' );
 Tracer::start($exporter);
 Grpc::load();
@@ -25,27 +36,21 @@ $instance_id = 'quickstart-instance-php';
 $table_id = 'bigtable-php-table';
 try {
 
-    $formattedTableName = Tracer::inSpan( [ 'name' => 'BigTableFormattedName' ] , function($bigtableClient,$instance_id, $table_id){
-        return $bigtableClient->tableName(getenv('PROJECT_ID'), $instance_id, $table_id);
-    },[$bigtableClient,$instance_id, $table_id]);
+    $formattedTableName = $bigtableClient->tableName(getenv('PROJECT_ID'), $instance_id, $table_id);
     // Read all responses until the stream is complete
-    $stream = Tracer::inSpan( [ 'name' => 'BigTableStream' ] , function($bigtableClient,$formattedTableName){
-        return $bigtableClient->readRows($formattedTableName);
-    },[$bigtableClient,$formattedTableName]);
-    Tracer::inSpan( [ 'name' => 'BigTableStreamForeach' ] , function($stream){
-        foreach ($stream->readAll() as $element) {
-            $iterator = iterator_to_array( $element->getChunks()->getIterator() );
-            foreach($iterator as $cellChunk){
-
-                //print_r( get_class_methods( $cellChunk ));
-                echo $cellChunk->getRowKey() . ' [ ' . $cellChunk->getValue() . ' ] ( ' . $cellChunk->getTimestampMicros() . ')';
-                echo "\n";
-            }
+    $stream = $bigtableClient->readRows($formattedTableName);
+    
+    foreach ($stream->readAll() as $element) {
+        $iterator = iterator_to_array( $element->getChunks()->getIterator() );
+        foreach($iterator as $cellChunk){
+            echo $cellChunk->getRowKey() . ' [ ' . $cellChunk->getValue() . ' ] ( ' . $cellChunk->getTimestampMicros() . ')'."\n";
         }
-    },[$stream]);
+    }
+
 } catch(ApiException $e){
-    print_r( $e );
+    echo $e->getMessage()."\n";
 } finally {
     $bigtableClient->close();
+    //$result = $traceClient->insert( $trace );
 }
 ?>
